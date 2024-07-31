833efb2ee23d26539e12af4a43e0e262c8b80044
sium <siumhaque1089@gmail.com>
2024-07-31 12:18:20 +0600

    2nd commit

---
+++ revision.js
@@ -0,0 +1,11 @@
+var bottle = 'water';
+var bottleColor = 'green';
+var sunglassPrice = 120;
+var penColor = 'blue';
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+var item3 = tableItems[3];
+
+tableItems[4] = 'notebook';
+console.log(tableItems);
