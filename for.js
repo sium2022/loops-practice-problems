61b594eff940f828dea75b51aaef82327e0a2aa7
sium <siumhaque1089@gmail.com>
2024-07-31 14:52:14 +0600

    7th commit

---
+++ for.js
@@ -0,0 +1,30 @@
+// while loop
+var roastGiven = 0;
+while (roastGiven < 7) {
+    // console.log('Roast den please');
+    roastGiven++
+}
+
+//for loop 
+for (var roastGiven = 0; roastGiven < 8; roastGiven++) {
+    //console.log('Roast den please');
+    
+}
+
+//simple version of for loop
+
+/* for(var i = 0; i < 7; i++){
+    console.log('simple for loop');
+    console.log(i);
+} */
+
+for (var i = 2; i <= 10; i+=2) {
+    console.log('even number');
+    console.log(i);
+    
+}
+
+for (var i = 1; i < 10; i+=2) {
+    console.log('odd number');
+    console.log(i);    
+}
