03abad81361e18d6b06fedb74f7d54cceeb141e3
sium <siumhaque1089@gmail.com>
2024-07-31 16:29:38 +0600

    9th commit

---
+++ continue.js
@@ -0,0 +1,17 @@
+/* var numbers = [45, 32, 46, 89, 68, 30, 75];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    if (number > 50) {
+        continue;        
+    }
+    console.log(number);
+} */
+
+    var items = ['bottle', 'mobile', 'laptop', 'key', 'mouse', 'keyboard'];
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if (item = 'key') {
+            continue;
+        }
+        console.log(item);
+    }
