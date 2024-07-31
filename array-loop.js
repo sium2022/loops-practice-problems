418a4725822c1866f9ce08bd078175cc014aa4b8
sium <siumhaque1089@gmail.com>
2024-07-31 15:42:46 +0600

    8th commit

---
+++ array-loop.js
@@ -0,0 +1,23 @@
+//targate: display every element of an array
+
+/* var numbers = [45, 87, 89, 56, 32, 51, 25];
+for (var i = 0; i < 7; i++) {
+    var number = numbers[i];
+    //console.log(number);
+    
+}
+
+let array = [45, 87, 89, 56, 32, 51, 25, 156, 151, 312];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    //console.log(element);
+    
+} */
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+
+for (var i = 0; i < items.length; i++) {
+    var element = items[i];
+    console.log(element);
+    
+}
