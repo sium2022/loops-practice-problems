c3d0fc5800c33d4010663f747267adfe34b2222b
sium <siumhaque1089@gmail.com>
2024-07-31 12:48:42 +0600

    3rd commit

---
+++ while.js
@@ -0,0 +1,13 @@
+var roastGiven = 0;
+while (roastGiven <= 450) {
+    console.log('Roast den please');
+console.log(roastGiven);
+    roastGiven++;
+}
+
+/*
+1. Loop variable
+2. condition in side the while loop
+3. Loop body
+4. Change loop variable condition
+*/
