6f6228d15d213d6539317c26fce8c8defde6a2b8
sium <siumhaque1089@gmail.com>
2024-09-05 08:25:16 +0600

    5th sept.

--- while.js
+++ while.js
@@ -1,13 +1,15 @@
-var roastGiven = 0;
-while (roastGiven <= 450) {
-    console.log('Roast den please');
-console.log(roastGiven);
-    roastGiven++;
-}
-
 /*
 1. Loop variable
 2. condition in side the while loop
 3. Loop body
 4. Change loop variable
 */
+
+
+
+let roastGiven = 0;
+while (roastGiven <= 5) {
+    console.log('Roast den please');
+    console.log(roastGiven);
+    roastGiven++;
+}
